dd5774c5267191779f95df39898db013d6a18ab9
cheungbh <534660436@qq.com>
2022-03-24 12:59:16 +0800

    add stat mom in mobilenet cifar

--- dynconv/models/resnet_32x32.py
+++ dynconv/models/resnet_32x32.py
@@ -16,7 +16,7 @@ from models.resnet_util import *
 ########################################
 
 class ResNet_32x32(nn.Module):
-    def __init__(self, layers, num_classes=10, pretrained=False, sparse=False,  mask_type="conv"):
+    def __init__(self, layers, num_classes=10, pretrained=False, sparse=False, **kwargs):
         super(ResNet_32x32, self).__init__()
 
         if pretrained is not False:
@@ -30,9 +30,9 @@ class ResNet_32x32(nn.Module):
         self.conv1 = conv3x3(3, 16)
         self.bn1 = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.layer1 = self._make_layer(block, 16, layers[0], mask_type=mask_type)
-        self.layer2 = self._make_layer(block, 32, layers[1], stride=2, mask_type=mask_type)
-        self.layer3 = self._make_layer(block, 64, layers[2], stride=2, mask_type=mask_type)
+        self.layer1 = self._make_layer(block, 16, layers[0], **kwargs)
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2, **kwargs)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2, **kwargs)
         self.avgpool = nn.AvgPool2d(8)
         self.fc = nn.Linear(64 * block.expansion, num_classes)
 
@@ -44,7 +44,7 @@ class ResNet_32x32(nn.Module):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
     
-    def _make_layer(self, block, planes, blocks, stride=1, mask_type="conv"):
+    def _make_layer(self, block, planes, blocks, stride=1, **kwargs):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
@@ -54,10 +54,10 @@ class ResNet_32x32(nn.Module):
             )
 
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample, sparse=self.sparse, mask_type=mask_type))
+        layers.append(block(self.inplanes, planes, stride, downsample, sparse=self.sparse, **kwargs))
         self.inplanes = planes * block.expansion
         for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes, sparse=self.sparse, mask_type=mask_type))
+            layers.append(block(self.inplanes, planes, sparse=self.sparse, **kwargs))
 
         return nn.Sequential(*layers)
 
