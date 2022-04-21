6cccc00011fd3c1d7bccaa01dbb167cff5ed3ad2
cheungbh <534660436@qq.com>
2022-04-21 08:56:04 +0800

    fix min to target cmd

--- dynconv/feature_mask_generate.py
+++ dynconv/feature_mask_generate.py
@@ -4,9 +4,9 @@ import os
 pretrain_path = "weight/resnet50/resnet50-19c8e357.pth"
 ratios = [0.1, 0.25, 0.5, 0.75, 0.9]
 mask_types = ["zero_top"]
-execute = False
+execute = True
 data_root = "/media/hkuit164/Elements1/imagenet"
-stages = [0, 1, 2, 3]
+stages = [[0,1,2], [0,1,3],[0,2,3],[1,2,3]]
 bs = 32
 target_folder = "feature_analysis"
 os.makedirs(target_folder, exist_ok=True)
@@ -14,11 +14,13 @@ os.makedirs(target_folder, exist_ok=True)
 for mask_type in mask_types:
     for ratio in ratios:
         for stage in stages:
+            stage_str = list(map(lambda x:str(x), stage))
+            stage_cmd_str, stage_file_str = " ".join(stage_str), ",".join(stage_str)
             cmd = "python main_imagenet.py --budget -1 --model resnet50 --mask_type {} --mask_thresh {} " \
-                  "--min_stage {} --dataset-root {} --load {} -e --batchsize {}".\
-                format(mask_type, ratio, stage, data_root, pretrain_path, bs)
+                  "--target_stage {} --dataset-root {} --load {} -e --batchsize {}".\
+                format(mask_type, ratio, stage_cmd_str, data_root, pretrain_path, bs)
             print(cmd)
             if execute:
-                target_file = os.path.join(target_folder, "{}-{}-min{}.txt".format(mask_type, ratio, stage))
+                target_file = os.path.join(target_folder, "{}-{}-target{}.txt".format(mask_type, ratio, stage_file_str))
                 if not os.path.exists(target_file):
                     os.system(cmd + " > " + target_file)
