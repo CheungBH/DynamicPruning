70932c33952958e8f9aa60971f5f7e87f5f682f5
cheungbh <534660436@qq.com>
2022-05-06 17:36:57 +0800

    add ?

--- dynconv/feature_mask_generate.py
+++ dynconv/feature_mask_generate.py
@@ -1,12 +1,12 @@
 
 import os
 
-pretrain_path = "weight/resnet50/resnet50-19c8e357.pth"
+pretrain_path = "weights/resnet50-0676ba61.pth"
 ratios = [0.4, 0.6, 0.1, 0.25, 0.5, 0.75, 0.9]
-mask_types = ["zero_top"]
+mask_types = ["sum", "abs_sum"]
 execute = True
-data_root = "/media/hkuit164/Elements1/imagenet"
-stages = [[0,1], [0,2], [0,3], [1,2], [1,3]]
+data_root = "/media/ssd0/imagenet/"
+stages = [[0], [1], [2], [3], [0,1,2], [0,1,3], [0,2,3], [1,2,3], [0,1,2,3]]
 bs = 32
 target_folder = "feature_analysis"
 os.makedirs(target_folder, exist_ok=True)
@@ -16,7 +16,7 @@ for mask_type in mask_types:
         for stage in stages:
             stage_str = list(map(lambda x:str(x), stage))
             stage_cmd_str, stage_file_str = " ".join(stage_str), ",".join(stage_str)
-            cmd = "python main_imagenet.py --budget -1 --model resnet50 --mask_type {} --mask_thresh {} " \
+            cmd = "CUDA_VISBLE_DEVICES=0 python main_imagenet.py --budget -1 --model resnet50 --mask_type {} --mask_thresh {} " \
                   "--target_stage {} --dataset-root {} --load {} -e --batchsize {}".\
                 format(mask_type, ratio, stage_cmd_str, data_root, pretrain_path, bs)
             print(cmd)
