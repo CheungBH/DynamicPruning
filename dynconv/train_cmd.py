57c6149ea3b6032a9f7a9f803db517db86b59d44
cheungbh <534660436@qq.com>
2022-05-19 10:15:28 +0800

    add dhsaiou

--- dynconv/train_cmd.py
+++ dynconv/train_cmd.py
@@ -1,9 +1,9 @@
 #-*-coding:utf-8-*-
 
 cmds = [
-    "CUDA_VISIBLE_DEVICES=0 python main_imagenet.py --model_cfg hardware --mask_kernel 1 --model resnet50 --no_attention --input_resolution --dataset-root /media/hkuit155/NewDisk/imagenet --batchsize 64 --budget 0.75 --load exp/imagenet_resnet50_hardware/baseline/checkpoint_best.pth --sparse_strategy static --sparse_weight 0 --valid_range 1 -s exp/add_conv1_mask/s75",
-    "CUDA_VISIBLE_DEVICES=1 python main_imagenet.py --model_cfg hardware --mask_kernel 1 --model resnet50 --no_attention --input_resolution --dataset-root /media/hkuit155/NewDisk/imagenet --batchsize 64 --budget 0.50 --load exp/imagenet_resnet50_hardware/baseline/checkpoint_best.pth --sparse_strategy static --sparse_weight 0 --valid_range 1 -s exp/add_conv1_mask/s50",
-    "CUDA_VISIBLE_DEVICES=2 python main_imagenet.py --model_cfg hardware --mask_kernel 1 --model resnet50 --no_attention --input_resolution --dataset-root /media/hkuit155/NewDisk/imagenet --batchsize 64 --budget 0.25 --load exp/imagenet_resnet50_hardware/baseline/checkpoint_best.pth --sparse_strategy static --sparse_weight 0 --valid_range 1 -s exp/add_conv1_mask/s75",
+    "CUDA_VISIBLE_DEVICES=0 python main_imagenet.py --model_cfg hardware --mask_kernel 1 --model resnet50 --no_attention --input_resolution --dataset-root /media/ssd0/imagenet --batchsize 80 --budget 0.75 --load exp/imagenet_1x1/baseline/checkpoint_best.pth --sparse_strategy static --sparse_weight 0 --valid_range 1 -s exp/add_conv1_mask/s75",
+    "CUDA_VISIBLE_DEVICES=1 python main_imagenet.py --model_cfg hardware --mask_kernel 1 --model resnet50 --no_attention --input_resolution --dataset-root /media/ssd0/imagenet --batchsize 80 --budget 0.50 --load exp/imagenet_1x1/baseline/checkpoint_best.pth --sparse_strategy static --sparse_weight 0 --valid_range 1 -s exp/add_conv1_mask/s50",
+    "CUDA_VISIBLE_DEVICES=2 python main_imagenet.py --model_cfg hardware --mask_kernel 1 --model resnet50 --no_attention --input_resolution --dataset-root /media/ssd0/imagenet --batchsize 80 --budget 0.25 --load exp/imagenet_1x1/baseline/checkpoint_best.pth --sparse_strategy static --sparse_weight 0 --valid_range 1 -s exp/add_conv1_mask/s25",
 
 ]
 
