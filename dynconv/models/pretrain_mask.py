485b5985b93e4521f9e16e0132a902c9751e61ca
cheungbh <534660436@qq.com>
2022-05-29 12:48:52 +0800

    fix feature size error

--- dynconv/models/pretrain_mask.py
+++ dynconv/models/pretrain_mask.py
@@ -28,13 +28,13 @@ class ZeroRatioTopMask:
         bs, c, h, w = feat.shape
         zero_mask = torch.sum((feat == 0).int(), dim=1)
         soft_mask = torch.true_divide(zero_mask, c)
-        if stride:
-            soft_mask = nn.functional.upsample_nearest(soft_mask.unsqueeze(dim=1), size=(int(h/2), int(w/2))).squeeze(dim=1)
+        # if stride:
+        soft_mask = nn.functional.upsample_nearest(soft_mask.unsqueeze(dim=1), size=(int(h/stride), int(w/stride))).squeeze(dim=1)
         if curr_block not in self.target_stage:
             return torch.ones_like(soft_mask)
         else:
             thresh = soft_mask.view(-1).sort()[0][int(soft_mask.numel()*self.threshold)]
-            return (soft_mask <= thresh).int()
+            return (soft_mask <= thresh).float()
 
 
 class SumNormalizeMask:
@@ -44,16 +44,16 @@ class SumNormalizeMask:
 
     def process(self, feat, stride, curr_block):
         summed_feat = torch.sum(feat, 1)
-        if stride:
-            b, c, h, w = feat.shape
-            summed_feat = nn.functional.upsample_nearest(summed_feat.unsqueeze(1), size=(int(h/2), int(w/2))).squeeze()
+        # if stride:
+        b, c, h, w = feat.shape
+        summed_feat = nn.functional.upsample_nearest(summed_feat.unsqueeze(1), size=(int(h/stride), int(w/stride))).squeeze()
         thresholds = summed_feat.view(feat.shape[0], -1).sort(dim=1)[0][:, int((summed_feat.shape[-1]*summed_feat.shape[-2]*self.threshold))]
         thresh_masks = thresholds.unsqueeze(dim=1).unsqueeze(dim=1).expand_as(summed_feat)
 
         if curr_block not in self.target_stage:
             return torch.ones_like(summed_feat)
         else:
-            return (summed_feat > thresh_masks).int()
+            return (summed_feat > thresh_masks).float()
 
 
 class AbsSumNormalizeMask:
@@ -63,16 +63,16 @@ class AbsSumNormalizeMask:
 
     def process(self, feat, stride, curr_block):
         summed_feat = torch.sum(torch.abs(feat), 1)
-        if stride:
-            b, c, h, w = feat.shape
-            summed_feat = nn.functional.upsample_nearest(summed_feat.unsqueeze(1), size=(int(h/2), int(w/2))).squeeze()
+        # if stride:
+        b, c, h, w = feat.shape
+        summed_feat = nn.functional.upsample_nearest(summed_feat.unsqueeze(1), size=(int(h/stride), int(w/stride))).squeeze()
         thresholds = summed_feat.view(feat.shape[0], -1).sort(dim=1)[0][:, int((summed_feat.shape[-1]*summed_feat.shape[-2]*self.threshold))]
         thresh_masks = thresholds.unsqueeze(dim=1).unsqueeze(dim=1).expand_as(summed_feat)
 
         if curr_block not in self.target_stage:
             return torch.ones_like(summed_feat)
         else:
-            return (summed_feat > thresh_masks).int()
+            return (summed_feat > thresh_masks).float()
 
 
 class NoneMask:
@@ -94,9 +94,8 @@ class RandomMask:
     def process(self, feat, stride, curr_block):
         bs, c, h, w = feat.shape
         rand_mask = torch.rand((bs, h, w)).cuda()
-        if stride:
-            rand_mask = nn.functional.upsample_nearest(rand_mask.unsqueeze(1), size=(int(h/2), int(w/2))).squeeze()
+        rand_mask = nn.functional.upsample_nearest(rand_mask.unsqueeze(1), size=(int(h/stride), int(w/stride))).squeeze()
         if curr_block not in self.target_stage:
             return torch.ones_like(rand_mask)
         else:
-            return (rand_mask > self.threshold).int()
+            return (rand_mask > self.threshold).float()
